d48f4deb6d2560eda468d46e2d7d5ed7bde4ade1
Nishita Amoli <nishita1oct@gmail.com>
2024-09-04 18:44:38 +0530

    Reverse_Integer.java

--- Reverse_Integer.java
+++ Reverse_Integer.java
@@ -1,6 +1,8 @@
+/* Q7- REVERSE INTEGER
+ Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+ */
 public class Reverse_Integer {
         public int reverse(int x) {
-            
             int rev=0;
             while(x!=0){
                 int digit=x%10;
