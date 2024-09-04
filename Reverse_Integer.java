587921d705dce5b3923026bb0bc3b915a31d5bb9
Nishita Amoli <nishita1oct@gmail.com>
2024-09-04 18:43:06 +0530

    Update Reverse_Integer.java
    Problem 7:- REVERSE INTEGER

--- Reverse_Integer.java
+++ Reverse_Integer.java
@@ -1,3 +1,16 @@
 public class Reverse_Integer {
-
+        public int reverse(int x) {
+            
+            int rev=0;
+            while(x!=0){
+                int digit=x%10;
+                int newrev=rev*10+digit;
+                if(newrev/10!=rev){
+                    return 0;
+                }
+                rev=newrev;
+                x/=10;
+            }
+            return rev;
+        }
 }
